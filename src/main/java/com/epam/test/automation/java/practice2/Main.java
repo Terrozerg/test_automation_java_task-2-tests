84c48aae10f6ad537614d98aaa8b2dce4f3a7357
Stanislav <Terrozerg@gmail.com>
2021-03-25 14:18:27 +0200

    1

--- src/main/java/com/epam/test/automation/java/practice2/Main.java
+++ src/main/java/com/epam/test/automation/java/practice2/Main.java
@@ -8,8 +8,16 @@ public class Main {
      * </summary>
      */
     public static int task1(int value) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        int result = 0;
+        while(value>0){
+            int temp = value%10;
+
+            if((temp&1)!=0){
+                result+=temp;
+            }
+            value=value/10;
+        }
+        return result;
     }
 
     /**
@@ -18,8 +26,16 @@ public class Main {
      * </summary>
      */
     public static int task2(int value) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        int result=0;
+        while(value>0){
+            int temp = value%2;
+
+            if(temp==1){
+               result++;
+            }
+            value=value/2;
+        }
+        return result;
     }
 
     /**
@@ -28,7 +44,22 @@ public class Main {
      * </summary>
      */
     public static int task3(int value) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if(value>45){
+            System.out.println("Integer overflow for value: "+value+", put value < 46.");
+            return -1;
+        }
+        int result = 0;
+        int last = 0;
+        int curr = 1;
+        while(value>0){
+            int temp = curr;
+            result+=last;
+
+            curr=curr+last;
+            last=temp;
+
+            value--;
+        }
+        return result;
     }
 }
--- src/test/java/com/epam/test/automation/java/practice2/MainTest.java
+++ src/test/java/com/epam/test/automation/java/practice2/MainTest.java
@@ -1,4 +1,50 @@
 package com.epam.test.automation.java.practice2;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class MainTest {
+    @Test
+    public void task1PassTest(){
+        Assert.assertEquals(4,Main.task1(1234));
+        Assert.assertEquals(0,Main.task1(246));
+    }
+
+    @Test
+    public void task1BordersTest(){
+        Assert.assertEquals(0,Main.task1(0));
+        Assert.assertEquals(0,Main.task1(-1));
+        Assert.assertEquals(18,Main.task1(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void task2PassTest(){
+        Assert.assertEquals(3,Main.task2(14));
+        Assert.assertEquals(1,Main.task2(128));
+    }
+
+    @Test
+    public void task2BordersTest(){
+        Assert.assertEquals(0,Main.task2(0));
+        Assert.assertEquals(0,Main.task2(-1));
+        Assert.assertEquals(31,Main.task2(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void task3PassTest(){
+        Assert.assertEquals(33,Main.task3(8));
+        Assert.assertEquals(143,Main.task3(11));
+    }
+
+    @Test
+    public void task3BordersTest(){
+        Assert.assertEquals(0,Main.task3(0));
+        Assert.assertEquals(0,Main.task3(1));
+        Assert.assertEquals(1836311902,Main.task3(45));
+    }
+
+    @Test
+    public void task3FailTest(){
+        Assert.assertEquals(-1,Main.task3(66));
+    }
 }
